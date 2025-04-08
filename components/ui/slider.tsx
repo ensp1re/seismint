51968ccb7c2963023a50f5f189a78453d641189e
ensp1re <ostapuk1318@gmail.com>
2025-04-08 19:57:53 +0300

    addded DEX

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -13,7 +13,8 @@ const Slider = React.forwardRef<
     ref={ref}
     className={cn(
       "relative flex w-full touch-none select-none items-center",
-      className
+      className || ""
+
     )}
     {...props}
   >
